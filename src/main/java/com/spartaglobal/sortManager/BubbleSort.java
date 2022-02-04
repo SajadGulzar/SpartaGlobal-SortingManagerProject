236c1748d1972428c89b6459c6497ee3b7a10fd7
Sajad <osajad_123@hotmail.co.uk>
2022-02-04 23:32:37 +0000

    Implementing the two Algorithms (BubbleSort/MergeSort)

---
+++ src/main/java/com/spartaglobal/sortManager/BubbleSort.java
@@ -0,0 +1,31 @@
+package com.spartaglobal.sortManager;
+
+public class BubbleSort implements Sorter {
+
+    @Override
+        public int[] sort(int[] arrayToSort) {
+            return sortArray(arrayToSort);
+        }
+
+        @Override
+        public int getLeft(int element)  {
+            return 0;
+        }
+
+        public int[] sortArray(int[] randomArray) {
+            int arrLength = randomArray.length;
+            for(int i=0; i < arrLength; i++) {
+                for (int j=0; j < arrLength - i - 1; j++) { // j = i
+                    if(randomArray[j] > (randomArray[j+1])) {
+                        int holdNum = randomArray[j];
+                        randomArray[j] = randomArray[j+1];
+                        randomArray[j+1] = holdNum;
+                    }
+                }
+            }
+            return randomArray;
+        }
+    }
+
+
+
---
+++ src/main/java/com/spartaglobal/sortManager/Sorter.java
@@ -0,0 +1,8 @@
+package com.spartaglobal.sortManager;
+
+public interface Sorter {
+    int[] sort(int[] arrayToSort);
+
+    int getLeft(int element) ;
+
+}
